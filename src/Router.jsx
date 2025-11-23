a5d9f571a4b1e369ede327dc8f4535a31d4656ff
Wang Li Xiang <khintdh31005@gmail.com>
2025-11-23 21:19:41 +0630

    add ProtectedRoute for ReturnPage and include Dashboard route

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,6 +6,8 @@ import ReturnPage from "./pages/Returnpage.jsx";
 import Login from './components/Auth/Login';
 import Signup from './components/Auth/Signup';
 import Profile from './components/Profile/Profile';
+import ProtectedRoute from "./components/Auth/ProtectedRoute";
+import Dashboard from "./pages/Admin/Dashboard";
 
 export default function AppRouter() {
   return (
@@ -13,10 +15,18 @@ export default function AppRouter() {
      
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/return" element={<ReturnPage />} />
+        <Route
+          path="/return"
+          element={
+            <ProtectedRoute>
+              <ReturnPage />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     
     </Router>
